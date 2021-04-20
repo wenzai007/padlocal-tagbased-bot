59b9012a126da0f987c77c89ed87b07c0066a568
KnightRider007 <zwzistheking@gmail.com>
2021-04-20 09:15:28 +0800

    new

---
+++ entity/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <div >
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h2>Welcome to React</h2>
+        </div>
+        <p >
+          To get started, edit  and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
